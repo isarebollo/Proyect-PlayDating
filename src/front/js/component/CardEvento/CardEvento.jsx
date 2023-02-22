1892e886f5cc2f3e7eab6abe9f21223de85a035c
Isabel Rebollo <95927789+isarebollo@users.noreply.github.com>
2023-02-22 11:55:51 +0100

    Boton ver detalles añadido a cardEventos

--- src/front/js/component/CardEvento/CardEvento.jsx
+++ src/front/js/component/CardEvento/CardEvento.jsx
@@ -54,15 +54,103 @@ export const CardEvento = (props) => {
 
         <div className="col">
           <div className="card h-100 mb-4 text-center">
+            <div className="card-footer">
+              <small className="text-muted">{props.estado}</small>
+            </div>
+
             <img src={props.src} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{props.name}</h5>
 
               <p className="card-text">{date}</p>
             </div>
-            <div className="card-footer">
-              <small className="text-muted">{props.estado}</small>
+            <div>
+
+
+              {props.creador ==
+                parseInt(localStorage.getItem("usuario"), 10) ? (
+                <div className="card-footer bg-body">
+                  <Link to={props.route}>
+                    <button
+
+                      href="#"
+                      className="btn btn-info"
+                      role="button"
+                    >
+                      Ver Detalles
+                    </button>
+                  </Link>
+                  {props.estado !== "Cancelado" && props.estado !== "Cerrado" && (
+                    <button
+                      onClick={() => {
+                        setModal1(true);
+                        setTextoModal(
+                          "¿Estás seguro de que quieres cancelar este evento?"
+                        );
+                      }}
+
+                      href="#"
+                      className="btn m-1 btn-danger"
+                      role="button"
+                    >
+                      Cancelar Evento
+                    </button>
+                  )}
+                </div>
+              ) : undefined !=
+                props.participantes.find(
+                  (participante) =>
+                    participante.id == localStorage.getItem("usuario")
+                ) ? (
+                <div>
+                  <div className="card-footer bg-body">
+                    <Link to={props.route}>
+                      <button
+                        id="buttonVerDetalles"
+                        href="#"
+                        className="btn"
+                        role="button"
+                      >
+                        Ver Detalles
+                      </button>
+                    </Link>
+                    {props.estado !== "Cancelado" &&
+                      props.estado !== "Cerrado" && (
+                        <button
+
+                          className="btn m-1"
+                          onClick={() => {
+                            setModal3(true);
+                            setTextoModal(
+                              "¿Estás seguro de que quieres retirar tu participación de este evento?"
+                            );
+                          }}
+                        >
+                          Retirarse
+                        </button>
+                      )}
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <div className="card-footer bg-body">
+                    <Link to={props.route}>
+                      <button
+
+                        href="#"
+                        className="btn btn-info"
+                        role="button"
+                      >
+                        Ver Detalles
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              )}
+
             </div>
+
+
           </div>
         </div>
 
@@ -88,3 +176,7 @@ CardEvento.propTypes = {
   estado: propTypes.string,
   fecha_y_hora: propTypes.string,
 };
+
+
+
+
